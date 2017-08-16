98f48e6798187ca299b3d720833ee0be0fb504ca
Reed Rosenbluth <reed.rosenbluth@gmail.com>
2017-08-15 22:14:13 -0600

    tabs to spaces

--- contracts/Billboard.sol
+++ contracts/Billboard.sol
@@ -2,46 +2,46 @@ pragma solidity ^0.4.4;
 
 contract Billboard {
 
-	uint public price;
-	address public author;
-	string public message;
+  uint public price;
+  address public author;
+  string public message;
 
-	mapping (address => uint) pendingWithdrawals;
+  mapping (address => uint) pendingWithdrawals;
 
-	function Billboard() payable {
-		message = "hello world";
-		price = msg.value;
-		author = msg.sender;
-	}
+  function Billboard() payable {
+    message = "hello world";
+    price = msg.value;
+    author = msg.sender;
+  }
 
-	function setMessage(string m) payable returns(bool) {
-		if (msg.value > price) {
-			pendingWithdrawals[author] += msg.value;
-			price = msg.value;
-			author = msg.sender;
-			message = m;
-			return true;
-		} else {
-		  return false;
-		}
-	}
+  function setMessage(string m) payable returns(bool) {
+    if (msg.value > price) {
+      pendingWithdrawals[author] += msg.value;
+      price = msg.value;
+      author = msg.sender;
+      message = m;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	function withdraw() {
-	  uint amount = pendingWithdrawals[msg.sender];
-	  pendingWithdrawals[msg.sender] = 0;
-	  msg.sender.transfer(amount);
-	}
+  function withdraw() {
+    uint amount = pendingWithdrawals[msg.sender];
+    pendingWithdrawals[msg.sender] = 0;
+    msg.sender.transfer(amount);
+  }
 
-	function getMessage() returns(string) {
-		return message;
-	}
+  function getMessage() returns(string) {
+    return message;
+  }
 
-	function getPrice() returns(uint) {
-		return price;
-	}
+  function getPrice() returns(uint) {
+    return price;
+  }
 
-	function getPendingWithdrawal() returns(uint) {
-		return pendingWithdrawals[msg.sender];
-	}
+  function getPendingWithdrawal() returns(uint) {
+    return pendingWithdrawals[msg.sender];
+  }
 
 }
